f2717389873205b1b6d941f3bacffcf3fc83e37b
juan-zv <juanzuritavasquez@gmail.com>
2025-10-29 12:26:17 -0600

    updated database with xxl sizes with cogs of 0 const COSTS: readonly { design: Design; type: ProductType; size: Size;

--- src/App.tsx
+++ src/App.tsx
@@ -27,77 +27,91 @@ const cogs = [
   { design: "doubt-not", type: "tshirt", size: "medium", cogs: 9.20 },
   { design: "doubt-not", type: "tshirt", size: "large", cogs: 9.58 },
   { design: "doubt-not", type: "tshirt", size: "xl", cogs: 9.36 },
+  { design: "doubt-not", type: "tshirt", size: "xxl", cogs: 0 },
 
   { design: "doubt-not", type: "hoodie", size: "small", cogs: 20.55 },
   { design: "doubt-not", type: "hoodie", size: "medium", cogs: 20.55 },
   { design: "doubt-not", type: "hoodie", size: "large", cogs: 20.55 },
   { design: "doubt-not", type: "hoodie", size: "xl", cogs: 20.55 },
+  { design: "doubt-not", type: "hoodie", size: "xxl", cogs: 0 },
 
   // Child of God
   { design: "child-of-god", type: "tshirt", size: "small", cogs: 8.88 },
   { design: "child-of-god", type: "tshirt", size: "medium", cogs: 8.68 },
   { design: "child-of-god", type: "tshirt", size: "large", cogs: 9.06 },
   { design: "child-of-god", type: "tshirt", size: "xl", cogs: 8.84 },
+  { design: "child-of-god", type: "tshirt", size: "xxl", cogs: 0 },
 
   { design: "child-of-god", type: "hoodie", size: "small", cogs: 20.03 },
   { design: "child-of-god", type: "hoodie", size: "medium", cogs: 20.03 },
   { design: "child-of-god", type: "hoodie", size: "large", cogs: 20.03 },
   { design: "child-of-god", type: "hoodie", size: "xl", cogs: 20.03 },
+  { design: "child-of-god", type: "hoodie", size: "xxl", cogs: 0 },
 
   // Line Upon Line
   { design: "line-upon-line", type: "tshirt", size: "small", cogs: 9.54 },
   { design: "line-upon-line", type: "tshirt", size: "medium", cogs: 9.34 },
   { design: "line-upon-line", type: "tshirt", size: "large", cogs: 9.72 },
   { design: "line-upon-line", type: "tshirt", size: "xl", cogs: 9.50 },
+  { design: "line-upon-line", type: "tshirt", size: "xxl", cogs: 0 },
 
   { design: "line-upon-line", type: "hoodie", size: "small", cogs: 20.69 },
   { design: "line-upon-line", type: "hoodie", size: "medium", cogs: 20.69 },
   { design: "line-upon-line", type: "hoodie", size: "large", cogs: 20.69 },
   { design: "line-upon-line", type: "hoodie", size: "xl", cogs: 20.69 },
+  { design: "line-upon-line", type: "hoodie", size: "xxl", cogs: 0 },
 
   // Hands of God
   { design: "hands-of-god", type: "tshirt", size: "small", cogs: 9.40 },
   { design: "hands-of-god", type: "tshirt", size: "medium", cogs: 9.20 },
   { design: "hands-of-god", type: "tshirt", size: "large", cogs: 9.58 },
   { design: "hands-of-god", type: "tshirt", size: "xl", cogs: 9.36 },
+  { design: "hands-of-god", type: "tshirt", size: "xxl", cogs: 0 },
 
   { design: "hands-of-god", type: "hoodie", size: "small", cogs: 20.55 },
   { design: "hands-of-god", type: "hoodie", size: "medium", cogs: 20.55 },
   { design: "hands-of-god", type: "hoodie", size: "large", cogs: 20.55 },
   { design: "hands-of-god", type: "hoodie", size: "xl", cogs: 20.55 },
+  { design: "hands-of-god", type: "hoodie", size: "xxl", cogs: 0 },
 
   // Endure to the End
   { design: "endure-to-the-end", type: "tshirt", size: "small", cogs: 9.27 },
   { design: "endure-to-the-end", type: "tshirt", size: "medium", cogs: 9.07 },
   { design: "endure-to-the-end", type: "tshirt", size: "large", cogs: 9.45 },
   { design: "endure-to-the-end", type: "tshirt", size: "xl", cogs: 9.23 },
+  { design: "endure-to-the-end", type: "tshirt", size: "xxl", cogs: 0 },
 
   { design: "endure-to-the-end", type: "hoodie", size: "small", cogs: 20.42 },
   { design: "endure-to-the-end", type: "hoodie", size: "medium", cogs: 20.42 },
   { design: "endure-to-the-end", type: "hoodie", size: "large", cogs: 20.42 },
   { design: "endure-to-the-end", type: "hoodie", size: "xl", cogs: 20.42 },
+  { design: "endure-to-the-end", type: "hoodie", size: "xxl", cogs: 0 },
 
   // Look to God
   { design: "look-to-god", type: "tshirt", size: "small", cogs: 9.38 },
   { design: "look-to-god", type: "tshirt", size: "medium", cogs: 9.18 },
   { design: "look-to-god", type: "tshirt", size: "large", cogs: 9.56 },
   { design: "look-to-god", type: "tshirt", size: "xl", cogs: 9.34 },
+  { design: "look-to-god", type: "tshirt", size: "xxl", cogs: 0 },
 
   { design: "look-to-god", type: "hoodie", size: "small", cogs: 20.53 },
   { design: "look-to-god", type: "hoodie", size: "medium", cogs: 20.53 },
   { design: "look-to-god", type: "hoodie", size: "large", cogs: 20.53 },
   { design: "look-to-god", type: "hoodie", size: "xl", cogs: 20.53 },
+  { design: "look-to-god", type: "hoodie", size: "xxl", cogs: 0 },
 
   // Death Has No Sting
   { design: "death-has-no-sting", type: "tshirt", size: "small", cogs: 9.54 },
   { design: "death-has-no-sting", type: "tshirt", size: "medium", cogs: 9.34 },
   { design: "death-has-no-sting", type: "tshirt", size: "large", cogs: 9.72 },
   { design: "death-has-no-sting", type: "tshirt", size: "xl", cogs: 9.50 },
+  { design: "death-has-no-sting", type: "tshirt", size: "xxl", cogs: 0 },
 
   { design: "death-has-no-sting", type: "hoodie", size: "small", cogs: 20.69 },
   { design: "death-has-no-sting", type: "hoodie", size: "medium", cogs: 20.69 },
   { design: "death-has-no-sting", type: "hoodie", size: "large", cogs: 20.69 },
   { design: "death-has-no-sting", type: "hoodie", size: "xl", cogs: 20.69 },
+  { design: "death-has-no-sting", type: "hoodie", size: "xxl", cogs: 0 },
 ] as const
 
 async function getData(targetDate: Date): Promise<Sale[]> {
